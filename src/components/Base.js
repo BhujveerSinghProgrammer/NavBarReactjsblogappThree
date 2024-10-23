db7cf98aef2b123586887a20c4850a986bc1314b
USER <USER@DESKTOP-GAP4S8J>
2024-10-23 17:23:23 +0530

    sddsdd

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,7 +2,7 @@ const Base = ({ title = "welcome to our website", children }) => {
   return (
     <div className="container-fluid">
       <h1>This is our header</h1>
-      {children}
+            {children}
       <h1>This is our Footer</h1>
     </div>
   );
