7c0df5f12d70b884f679074026fd4c362dcf0b00
USER <USER@DESKTOP-GAP4S8J>
2024-10-23 15:09:01 +0530

    ssss

---
+++ src/components/About.jsx
@@ -0,0 +1,5 @@
+const About=()=>{
+  return <h1>this is my About Component</h1>
+};
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import About from './components/About';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <About/>
+     <App />
+    {/* <About/> */}
   </React.StrictMode>
 );
 
